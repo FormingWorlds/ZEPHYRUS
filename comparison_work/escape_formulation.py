fd494f67552237a7a6b5691af0050c1de5c67702
Laurent Soucasse <135331272+lsoucasse@users.noreply.github.com>
2024-09-04 09:27:44 +0200

    Cleanup escape functions.

--- comparison_work/escape_formulation.py
+++ comparison_work/escape_formulation.py
@@ -3,13 +3,9 @@ import matplotlib.pyplot as plt
 
 import mors
 
-import sys
-import os
-zephyrus_dir = os.path.dirname('../src/zephyrus/')
-sys.path.extend([zephyrus_dir])
-from constants import *
-from planets_parameters import *
-from escape import *
+from zephyrus.constants import *
+from zephyrus.planets_parameters import *
+from zephyrus.escape import *
 
 ########################### Path to directories ###############################
 
@@ -40,8 +36,8 @@ Sun_Fxuv    = (Sun_Lxuv/(4 * np.pi * a_earth*au2cm **2)) * ergcm2stoWm2     # XU
 ########################### Escape computations ####################################
 
 for mass in planet_masses : 
-    zephyrus    = EL_escape('no',a_earth*au2m,e_earth,mass*Me,Ms,epsilon,Re,Re,Fxuv_earth_today)
-    attia_2021  = (dMdt_EL_Attia2021('no',a_earth*au2cm,e_earth,mass*Me*1e3,Ms*1e3,Re*1e2,Re*1e2,(Fxuv_earth_today/ergcm2stoWm2)*4*np.pi*a_earth*au2cm**2,Fxuv_earth_today/ergcm2stoWm2,epsilon,'no')/1e3)
+    zephyrus    = EL_escape(False,a_earth*au2m,e_earth,mass*Me,Ms,epsilon,Re,Re,Fxuv_earth_today)
+    attia_2021  = (dMdt_EL_Attia2021(False,a_earth*au2cm,e_earth,mass*Me*1e3,Ms*1e3,Re*1e2,(Fxuv_earth_today/ergcm2stoWm2)*4*np.pi*a_earth*au2cm**2,Fxuv_earth_today/ergcm2stoWm2,Rxuv=Re*1e2,epsilon=epsilon)/1e3)
     isofate     = (dMdt_Cherubim2024(mass*Me,Re,epsilon,Fxuv_earth_today)) * (4 * np.pi * Re ** 2) 
 
     escape_today.append({
@@ -51,8 +47,8 @@ for mass in planet_masses :
     })
 
 for fxuv in Sun_Fxuv : 
-    zephyrus    = EL_escape('no',a_earth*au2m,e_earth,Me,Ms,epsilon,Re,Re,fxuv)
-    attia_2021  = (dMdt_EL_Attia2021('no',a_earth*au2cm,e_earth,Me*1e3,Ms*1e3,Re*1e2,Re*1e2,(fxuv/ergcm2stoWm2)*4*np.pi*a_earth*au2cm**2,Fxuv_earth_today/ergcm2stoWm2,epsilon,'no')/1e3)
+    zephyrus    = EL_escape(False,a_earth*au2m,e_earth,Me,Ms,epsilon,Re,Re,fxuv)
+    attia_2021  = (dMdt_EL_Attia2021(False,a_earth*au2cm,e_earth,Me*1e3,Ms*1e3,Re*1e2,(fxuv/ergcm2stoWm2)*4*np.pi*a_earth*au2cm**2,Fxuv_earth_today/ergcm2stoWm2,Rxuv=Re*1e2,epsilon=epsilon)/1e3)
     isofate     = (dMdt_Cherubim2024(Me,Re,epsilon,fxuv)) * (4 * np.pi * Re ** 2) 
 
     escape_evolution.append({
