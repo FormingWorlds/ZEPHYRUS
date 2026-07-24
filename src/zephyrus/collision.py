fix mass loss calculation

src/zephyrus/collision.py
@@ -1,4 +1,3 @@
-#mass loss function during impacts
 """
 !!! info "`collision.py`"
     Function to compute fractional atmospheric loss during giant impacts.<br>
@@ -43,8 +42,9 @@ def mass_loss(v_c:float, M_i:float, M_T:float, rho_i:float, rho_t:float, R_i:flo
     #interacting volume, here we assume it is the same as the interacting mass of the colliding pair 
     fm = 1/4*((R_t + R_i)**3/(R_t**3 + R_i**3)) * (1-b)**2 * (1 + 2*b)
 
-    v_esc = np.sqrt((2*G * (M_T + M_i))/(R_t + R_i)) #escape velocity
+    v_esc = np.sqrt((2*G * (M_t + M_i))/(R_t + R_i)) #escape velocity
+    M_tot = M_i + M_t
 
-    bracket = (v_c/v_esc)**2 * (M_i/M_T)**(1/2) * (rho_i/rho_t)**(1/2) * fm 
+    bracket = (v_c/v_esc)**2 * (M_i/M_tot)**(1/2) * (rho_i/rho_t)**(1/2) * fm 
     mass_loss = 0.64 * bracket**0.65 #fractional atmospheric mass loss of target body, empirical (Kerregeis et al 2020)
     return mass_loss